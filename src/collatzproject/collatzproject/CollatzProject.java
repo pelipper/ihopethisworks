b7d7373695b09440363e99b2e0e477ef0eab6f07
errpoulos <erpoulos@unca.edu>
2018-05-06 20:17:42 -0400

    Program created all CSV files.

--- src/collatzproject/collatzproject/CollatzProject.java
+++ src/collatzproject/collatzproject/CollatzProject.java
@@ -13,59 +13,41 @@ public class CollatzProject {
     public static void main(String[] args) throws IOException {
         //initializations
         int lower = 1;
-        int upper = 11;
+        int upper = 100;
 
         CsvUtils csvUtils = new CsvUtils();
-        FileWriter csvWriterNoPredicate = new FileWriter(new File("src/csvOut/noPredicate.csv"));
-        FileWriter csvWriterPrimes = new FileWriter(new File("src/csvOut/primes.csv"));
-        FileWriter csvWriterPower = new FileWriter(new File("src/csvOut/power.csv"));
-        FileWriter csvWriterMult = new FileWriter(new File("src/csvOut/mult.csv"));
-        RealCollatz realCollatz = new RealCollatz(lower, upper);
+        FileWriter csvWriterNoPredicate = new FileWriter(new File("src/csvOut/primes.csv"));
         CollatzProxy collatzProxy = new CollatzProxy(lower, upper);
 
-
         csvUtils.writeLine(csvWriterNoPredicate, Arrays.asList("Initial Numbers : Collatz Number: Calculations "));
+
         List<String> initialNumbers = new ArrayList<>();
         List<String> collatzNumbers = new ArrayList<>();
         List<String> calculationSteps = new ArrayList<>();
         List<String> values = new ArrayList<>();
 
-
-        for (Integer i : collatzProxy.getCollatzNumbers(lower, upper).getInitialInts()
-                ) {
+        for (Integer i : collatzProxy.getCollatzNumbers(lower, upper, "Prime").getInitialInts()) {
             initialNumbers.add(i.toString());
-
         }
 
-
-        for (Integer i : collatzProxy.getCollatzNumbers(lower, upper).getCollatzNums()
-                ) {
+        for (Integer i : collatzProxy.getCollatzNumbers(lower, upper).getCollatzNums()) {
             collatzNumbers.add(i.toString());
-
         }
 
-        for (CollatzCalculation c : collatzProxy.getCalculations(lower, upper)) {
+        for (CollatzCalculation c : collatzProxy.getCalculations(lower, upper, "Prime")) {
             calculationSteps.add(c.calculationSteps().toString());
-
         }
 
-
-        for (int z = 0; z < collatzNumbers.size(); z++) {
+        for (int z = 0; z < calculationSteps.size(); z++) {
             values.add(initialNumbers.get(z));
             values.add(collatzNumbers.get(z));
             values.add(calculationSteps.get(z));
-            csvUtils.writeLine(csvWriterNoPredicate, values, ':', '\'');
+            csvUtils.writeLine(csvWriterNoPredicate, values, ':', '\u0000');
             values.remove(initialNumbers.get(z));
             values.remove(collatzNumbers.get(z));
             values.remove(calculationSteps.get(z));
             values.remove(":");
         }
-
-
-
-
-        System.out.println(values.size());
-
         csvWriterNoPredicate.flush();
         csvWriterNoPredicate.close();
 
